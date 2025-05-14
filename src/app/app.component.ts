d4e5d17fd60f35b871a4ace44681fe593285c330
Vitor Cesar <vitor18cesar1@gmail.com>
2025-05-14 15:21:02 -0300

    feat: adição sidebar inicial, style: estilização para guia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'crud';
+  sidebarOpen = true;
+  toggleSidebar(){
+    this.sidebarOpen = this.sidebarOpen? false : true;
+  }
 }
